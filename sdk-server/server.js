60a3665cdcbd03cdd5fc1472652f0535b71a0d69
Filip Muki Dobranic <filipdobranic@gmail.com>
2018-07-30 11:50:48 +0200

    new OG images for ads again

--- sdk-server/server.js
+++ sdk-server/server.js
@@ -43,11 +43,11 @@ app.get('/oglas/:oglasId', async ( req, res ) => {
       <meta property="og:type" content="article" />
       <meta property="og:title" content="${ads[parseInt(req.params.oglasId)].og_title}" />
       <meta property="og:description" content="${ads[parseInt(req.params.oglasId)].og_description}" />
-      <meta property="og:image" content="https://sestavivlado.si/static/og-vsi.png" />
+      <meta property="og:image" content="https://sestavivlado.si/static/${ads[parseInt(req.params.oglasId)].og_image}" />
       
       <meta name="twitter:title" content="${ads[parseInt(req.params.oglasId)].og_title}" />
       <meta name="twitter:description" content="${ads[parseInt(req.params.oglasId)].og_description}" />
-      <meta name="twitter:image" content="https://sestavivlado.si/static/og-vsi.png /">
+      <meta name="twitter:image" content="https://sestavivlado.si/static/${ads[parseInt(req.params.oglasId)].og_image} /">
     `);
 
     res.send($.html());
