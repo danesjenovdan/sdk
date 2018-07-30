d53f48058e93999283624e3947a5f057604b211e
Filip Muki Dobranic <filipdobranic@gmail.com>
2018-07-30 14:03:58 +0200

    share counter

--- src/components/pages/Oglasi.vue
+++ src/components/pages/Oglasi.vue
@@ -122,8 +122,8 @@ export default {
   },
 
   mounted() {
-    this.$http.get(`https://graph.facebook.com/?id=${document.location.href}`).then((response) => {
-      this.current = response.body.share.share_count;
+    this.$http.get(`https://graph.facebook.com/?id=https://sestavivlado.si/`).then((response) => {
+      this.current = response.body.share.share_count + 0;
     });
   },
 };
